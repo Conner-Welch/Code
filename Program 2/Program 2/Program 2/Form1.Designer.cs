03fd630f8cc6e6fb3ff9b96a0228119f84ae9c0b
Conner-Welch <116229257+Conner-Welch@users.noreply.github.com>
2022-10-19 18:42:51 -0400

    Add files via upload

---
+++ Program 2/Program 2/Program 2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeightLbl = new System.Windows.Forms.Label();
+            this.DistanceLbl = new System.Windows.Forms.Label();
+            this.DaysLbl = new System.Windows.Forms.Label();
+            this.WeightTxt = new System.Windows.Forms.TextBox();
+            this.DistanceTxt = new System.Windows.Forms.TextBox();
+            this.DaysTxt = new System.Windows.Forms.TextBox();
+            this.CompALbl = new System.Windows.Forms.Label();
+            this.CompBLbl = new System.Windows.Forms.Label();
+            this.CompCLbl = new System.Windows.Forms.Label();
+            this.CompAPrice = new System.Windows.Forms.Label();
+            this.CompBPrice = new System.Windows.Forms.Label();
+            this.CompCPrice = new System.Windows.Forms.Label();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.ResultLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WeightLbl
+            // 
+            this.WeightLbl.AutoSize = true;
+            this.WeightLbl.Location = new System.Drawing.Point(31, 35);
+            this.WeightLbl.Name = "WeightLbl";
+            this.WeightLbl.Size = new System.Drawing.Size(87, 13);
+            this.WeightLbl.TabIndex = 0;
+            this.WeightLbl.Text = "Package Weight";
+            // 
+            // DistanceLbl
+            // 
+            this.DistanceLbl.AutoSize = true;
+            this.DistanceLbl.Location = new System.Drawing.Point(30, 69);
+            this.DistanceLbl.Name = "DistanceLbl";
+            this.DistanceLbl.Size = new System.Drawing.Size(82, 13);
+            this.DistanceLbl.TabIndex = 1;
+            this.DistanceLbl.Text = "Distance (Miles)";
+            // 
+            // DaysLbl
+            // 
+            this.DaysLbl.AutoSize = true;
+            this.DaysLbl.Location = new System.Drawing.Point(30, 107);
+            this.DaysLbl.Name = "DaysLbl";
+            this.DaysLbl.Size = new System.Drawing.Size(72, 13);
+            this.DaysLbl.TabIndex = 2;
+            this.DaysLbl.Text = "Delivery Days";
+            // 
+            // WeightTxt
+            // 
+            this.WeightTxt.Location = new System.Drawing.Point(151, 28);
+            this.WeightTxt.Name = "WeightTxt";
+            this.WeightTxt.Size = new System.Drawing.Size(108, 20);
+            this.WeightTxt.TabIndex = 3;
+            // 
+            // DistanceTxt
+            // 
+            this.DistanceTxt.Location = new System.Drawing.Point(151, 62);
+            this.DistanceTxt.Name = "DistanceTxt";
+            this.DistanceTxt.Size = new System.Drawing.Size(108, 20);
+            this.DistanceTxt.TabIndex = 4;
+            // 
+            // DaysTxt
+            // 
+            this.DaysTxt.Location = new System.Drawing.Point(151, 100);
+            this.DaysTxt.Name = "DaysTxt";
+            this.DaysTxt.Size = new System.Drawing.Size(107, 20);
+            this.DaysTxt.TabIndex = 5;
+            // 
+            // CompALbl
+            // 
+            this.CompALbl.AutoSize = true;
+            this.CompALbl.Location = new System.Drawing.Point(303, 30);
+            this.CompALbl.Name = "CompALbl";
+            this.CompALbl.Size = new System.Drawing.Size(61, 13);
+            this.CompALbl.TabIndex = 6;
+            this.CompALbl.Text = "Company A";
+            // 
+            // CompBLbl
+            // 
+            this.CompBLbl.AutoSize = true;
+            this.CompBLbl.Location = new System.Drawing.Point(303, 65);
+            this.CompBLbl.Name = "CompBLbl";
+            this.CompBLbl.Size = new System.Drawing.Size(61, 13);
+            this.CompBLbl.TabIndex = 7;
+            this.CompBLbl.Text = "Company B";
+            // 
+            // CompCLbl
+            // 
+            this.CompCLbl.AutoSize = true;
+            this.CompCLbl.Location = new System.Drawing.Point(303, 103);
+            this.CompCLbl.Name = "CompCLbl";
+            this.CompCLbl.Size = new System.Drawing.Size(61, 13);
+            this.CompCLbl.TabIndex = 8;
+            this.CompCLbl.Text = "Company C";
+            // 
+            // CompAPrice
+            // 
+            this.CompAPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CompAPrice.Location = new System.Drawing.Point(384, 27);
+            this.CompAPrice.Name = "CompAPrice";
+            this.CompAPrice.Size = new System.Drawing.Size(92, 16);
+            this.CompAPrice.TabIndex = 9;
+            // 
+            // CompBPrice
+            // 
+            this.CompBPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CompBPrice.Location = new System.Drawing.Point(383, 63);
+            this.CompBPrice.Name = "CompBPrice";
+            this.CompBPrice.Size = new System.Drawing.Size(92, 18);
+            this.CompBPrice.TabIndex = 10;
+            // 
+            // CompCPrice
+            // 
+            this.CompCPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CompCPrice.Location = new System.Drawing.Point(383, 99);
+            this.CompCPrice.Name = "CompCPrice";
+            this.CompCPrice.Size = new System.Drawing.Size(91, 17);
+            this.CompCPrice.TabIndex = 11;
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Location = new System.Drawing.Point(156, 164);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(271, 34);
+            this.CalcButton.TabIndex = 12;
+            this.CalcButton.Text = "Calculate";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // ResultLbl
+            // 
+            this.ResultLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultLbl.Location = new System.Drawing.Point(162, 138);
+            this.ResultLbl.Name = "ResultLbl";
+            this.ResultLbl.Size = new System.Drawing.Size(253, 26);
+            this.ResultLbl.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.CalcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 377);
+            this.Controls.Add(this.ResultLbl);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.CompCPrice);
+            this.Controls.Add(this.CompBPrice);
+            this.Controls.Add(this.CompAPrice);
+            this.Controls.Add(this.CompCLbl);
+            this.Controls.Add(this.CompBLbl);
+            this.Controls.Add(this.CompALbl);
+            this.Controls.Add(this.DaysTxt);
+            this.Controls.Add(this.DistanceTxt);
+            this.Controls.Add(this.WeightTxt);
+            this.Controls.Add(this.DaysLbl);
+            this.Controls.Add(this.DistanceLbl);
+            this.Controls.Add(this.WeightLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WeightLbl;
+        private System.Windows.Forms.Label DistanceLbl;
+        private System.Windows.Forms.Label DaysLbl;
+        private System.Windows.Forms.TextBox WeightTxt;
+        private System.Windows.Forms.TextBox DistanceTxt;
+        private System.Windows.Forms.TextBox DaysTxt;
+        private System.Windows.Forms.Label CompALbl;
+        private System.Windows.Forms.Label CompBLbl;
+        private System.Windows.Forms.Label CompCLbl;
+        private System.Windows.Forms.Label CompAPrice;
+        private System.Windows.Forms.Label CompBPrice;
+        private System.Windows.Forms.Label CompCPrice;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Label ResultLbl;
+    }
+}
+
---
+++ Program 2/Program 2/Program 2/Form1.cs
@@ -0,0 +1,322 @@
+﻿// Grade ID: R7639
+// Program #: 2
+// Due Date: 10/15/2020
+// This Program Calculates the total cost of three different companies
+// After calculating each companies total it gives you the lowest cost company
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            int weight, distance, days; // variables of weight, distance, days
+            string lowestcost; // allows input of string 
+
+            const int MIN_WEIGHT = 0; // Minimum Weight
+            const int MIN_DISTANCE = 0; // Minimum Distance
+            const int MIN_DAYS = 0; // Minimum Days
+
+            const double aWeight = 1; // These are the const variables used to compare the cost of Company A, including the multiple Days
+            const double aDistance = .02;
+            const double aDAY_ONE = 20;
+            const double aDAY_TWO = 17;
+            const double aDay_THREE = 15;
+            const double aDAY_FOUR_SEVEN = 10;
+            const double aDAY_EIGHTPLUS = 7;
+
+            const double bWEIGHT_UNDER_TEN = 3; // These are used for Company B to give the accurate price
+            const double bWEIGHT_TEN_FIFTY = 5;
+            const double bWEIGHT_FIFTY_HUNDRED = 10;
+            const double bWEIGHT_HUNDRED_TWOHUNDRED = 20;
+            const double bWEIGHT_MORE = .15;
+            const double bDISTANCE = .10;
+            const double bDAYS_ONE_FOUR = 10;
+            const double bDAYS_MORE = 7;
+
+            const double cWEIGHT = .25; // These are used for Company C to get the accurate prices without magic numbers
+            const double cDISTANCE_MORE = 40;
+            const double cDISTANCE_750_1000 = 35;
+            const double CDISTANCE_500_750 = 25;
+            const double cDISTANCE_200_500 = 15;
+            const double cDISTANCE_LESS = 10;
+            const double cDAYS = 20;
+
+
+            double priceA = 0; // Allows comparison for the result label
+            double priceB = 0; 
+            double priceC = 0;
+
+            // below starts company A
+            if (int.TryParse(WeightTxt.Text, out weight) && (weight > MIN_WEIGHT)) // Allows input of weight 
+            {
+                if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                {
+                    if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                    {
+                        if (days == 1) // The following statements allow accurate pricing considering days to be shipped
+                        {
+                            priceA = (weight * aWeight) + (distance * aDistance) + aDAY_ONE; // gets the price based on the days shipped
+                            CompAPrice.Text = $"{priceA:C}"; // inputs price as currency in the Company A Result
+                        }
+                        else if (days == 2)
+                        {
+                            priceA = (weight * aWeight) + (distance * aDistance) + aDAY_TWO; // gets the price based on the days shipped
+                            CompAPrice.Text = $"{priceA:C}"; // inputs price as currency in the Company A Result
+                        }
+                        else if (days == 3)
+                        {
+                            priceA = (weight * aWeight) + (distance * aDistance) + aDay_THREE; // gets the price based on the days shipped
+                            CompAPrice.Text = $"{priceA:C}"; // inputs price as currency in the Company A Result
+                        }
+                        else if (days >= 4 && days <= 7)
+                        {
+                            priceA = (weight * aWeight) + (distance * aDistance) + aDAY_FOUR_SEVEN; // gets the price based on the days shipped
+                            CompAPrice.Text = $"{priceA:C}"; // inputs price as currency in the Company A Result
+                        }
+                        else
+                        {
+                            priceA = (weight * aWeight) + (distance * aDistance) + aDAY_EIGHTPLUS; // gets the price based on the days shipped
+                            CompAPrice.Text = $"{priceA:C}"; // inputs price as currency in the Company A Result
+                        }
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Days"); // invalid days
+                }
+                else
+                    MessageBox.Show("Enter Valid Distance"); // invalid distance
+            }
+            else
+                MessageBox.Show("Enter Valid Weight"); // invalid weight
+
+
+            // below this starts Company B 
+            if (int.TryParse(WeightTxt.Text, out weight) && (weight > MIN_WEIGHT)) // allows input of weight
+            {
+                if (weight < 10) // if weight is less than 10
+                {
+                    if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            if (days >= 1 && days <= 4) // if days between 1 and 4
+                            {
+                                priceB = bWEIGHT_UNDER_TEN + (distance * bDISTANCE) + bDAYS_ONE_FOUR; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                            else // days greater than 4 
+                            {
+                                priceB = bWEIGHT_UNDER_TEN + (distance * bDISTANCE) + bDAYS_MORE; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Distance"); // invalid distance
+                }
+                else if (weight >= 10 && weight < 50) // weight between 10 - 50
+                {
+                    if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            if (days >= 1 && days <= 4) // if days between 1 and 4
+                            {
+                                priceB = bWEIGHT_TEN_FIFTY + (distance * bDISTANCE) + bDAYS_ONE_FOUR; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                            else // days greater than 4
+                            {
+                                priceB = bWEIGHT_TEN_FIFTY + (distance * bDISTANCE) + bDAYS_MORE; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Distance"); // invalid distance
+                }
+                else if (weight >= 50 && weight < 100) // weight between 50 - 100
+                {
+                    if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            if (days >= 1 && days <= 4) // if days between 1 and 4
+                            {
+                                priceB = bWEIGHT_FIFTY_HUNDRED + (distance * bDISTANCE) + bDAYS_ONE_FOUR; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                            else // days greater than 4
+                            {
+                                priceB = bWEIGHT_FIFTY_HUNDRED + (distance * bDISTANCE) + bDAYS_MORE; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Distance"); // invalid distance
+                }
+                else if (weight >= 100 && weight < 200) // weight between 100 - 200
+                {
+                    if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            if (days >= 1 && days <= 4) // if days between 1 and 4
+                            {
+                                priceB = bWEIGHT_HUNDRED_TWOHUNDRED + (distance * bDISTANCE) + bDAYS_ONE_FOUR; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                            else // days greater than 4
+                            {
+                                priceB = bWEIGHT_HUNDRED_TWOHUNDRED + (distance * bDISTANCE) + bDAYS_MORE; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Distance"); // invalid distance
+                }
+                else // weight greater than 200
+                {
+                    if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            if (days >= 1 && days <= 4) // if days between 1 and 4
+                            {
+                                priceB = (weight * bWEIGHT_MORE) + (distance * bDISTANCE) + bDAYS_ONE_FOUR; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                            else // days greater than 4
+                            {
+                                priceB = (weight * bWEIGHT_MORE) + (distance * bDISTANCE) + bDAYS_MORE; // caluclates total
+                                CompBPrice.Text = $"{priceB:C}"; // places total in Company B result
+                            }
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else
+                        MessageBox.Show("Enter Valid Distance"); // invalid distance
+                }
+            }
+            else
+                MessageBox.Show("Enter Valid Weight"); // invalid weight
+
+
+            // below starts company C
+            if (int.TryParse(WeightTxt.Text, out weight) && (weight > MIN_WEIGHT)) // allows input of weight
+            {
+                if (int.TryParse(DistanceTxt.Text, out distance) && (distance > MIN_DISTANCE)) // allows input of distance
+                {
+                    if (distance < 200) // distance less than 200
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            priceC = (weight * cWEIGHT) + cDISTANCE_LESS + cDAYS; // caluclates total
+                            CompCPrice.Text = $"{priceC:C}"; // places total in Company C result
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else if (distance >= 200 && distance < 500) // distance between 200 - 500
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            priceC = (weight * cWEIGHT) + cDISTANCE_200_500 + cDAYS; // caluclates total
+                            CompCPrice.Text = $"{priceC:C}"; // places total in Company C result
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else if (distance >= 500 && distance < 750) // distance between 500 - 750
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            priceC = (weight * cWEIGHT) + CDISTANCE_500_750 + cDAYS; // caluclates total
+                            CompCPrice.Text = $"{priceC:C}"; // places total in Company C result
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else if (distance >= 750 && distance < 1000) // distance between 750 - 1000
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            priceC = (weight * cWEIGHT) + cDISTANCE_750_1000 + cDAYS; // caluclates total
+                            CompCPrice.Text = $"{priceC:C}"; // places total in Company C result
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                    else // distance greater than 1000
+                    {
+                        if (int.TryParse(DaysTxt.Text, out days) && (days > MIN_DAYS)) // allows input of days
+                        {
+                            priceC = (weight * cWEIGHT) + cDISTANCE_MORE + cDAYS; // caluclates total
+                            CompCPrice.Text = $"{priceC:C}"; // places total in Company C result
+                        }
+                        else
+                            MessageBox.Show("Enter Valid Days"); // invalid days
+                    }
+                }
+                else
+                    MessageBox.Show("Enter Valid Distance"); // invalid distance
+            }
+            else
+                MessageBox.Show("Enter Valid Weight"); // invalid weight
+
+           
+          if (priceA < priceB && priceA < priceC) // sees if priceA is the lowest 
+            {
+                lowestcost = "A"; // makes the lowest cost Company A
+                ResultLbl.Text = $"The Lowest Cost Company is {lowestcost}"; // places result in the result label
+            }
+          else if (priceB < priceA && priceB < priceC) // sees if priceB is the lowest
+            {
+                lowestcost = "B"; // makes the lowest cost company B
+                ResultLbl.Text = $"The Lowest Cost Company is {lowestcost}"; // places result in the result label
+            }
+          else if ( priceC < priceA && priceC < priceB) // sees if priceC is the lowest
+            {
+                lowestcost = "C"; // makes the lowest cost company C
+                ResultLbl.Text = $"The Lowest Cost Company is {lowestcost}"; // places result in the result label
+            }
+          else // if there is no clear lowest price
+            {
+                lowestcost = "Tie"; // makes the lowest cost a tie
+                ResultLbl.Text = $"The Lowest Cost Company is a {lowestcost}"; // places result in the result label
+            }
+
+
+
+            
+                
+        }
+    }
+}
