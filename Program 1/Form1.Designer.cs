30c3e1fd33bbb600c28b0e758afaf6da6c1874c4
Conner-Welch <116229257+Conner-Welch@users.noreply.github.com>
2022-10-19 18:40:14 -0400

    Add files via upload

---
+++ Program 1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Program_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MaxWidthLbl = new System.Windows.Forms.Label();
+            this.MaxLengthLbl = new System.Windows.Forms.Label();
+            this.HardwoodPriceLbl = new System.Windows.Forms.Label();
+            this.UnderlaymentLbl = new System.Windows.Forms.Label();
+            this.RoomLbl = new System.Windows.Forms.Label();
+            this.MaxWidthTxtBox = new System.Windows.Forms.TextBox();
+            this.MaxLengthTxtBox = new System.Windows.Forms.TextBox();
+            this.HardwoodPriceTxtBox = new System.Windows.Forms.TextBox();
+            this.UnderlaymentTxtBox = new System.Windows.Forms.TextBox();
+            this.FirstRoomTxtBox = new System.Windows.Forms.TextBox();
+            this.SqYardsLbl = new System.Windows.Forms.Label();
+            this.HardwoodCostLbl = new System.Windows.Forms.Label();
+            this.UnderlayCostLbl = new System.Windows.Forms.Label();
+            this.LaborLbl = new System.Windows.Forms.Label();
+            this.TotalCostLbl = new System.Windows.Forms.Label();
+            this.TotalSqYards = new System.Windows.Forms.Label();
+            this.TotalHardwoodCost = new System.Windows.Forms.Label();
+            this.TotalUnderlayCost = new System.Windows.Forms.Label();
+            this.TotalLaborCost = new System.Windows.Forms.Label();
+            this.GrandTotalCost = new System.Windows.Forms.Label();
+            this.CalcBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MaxWidthLbl
+            // 
+            this.MaxWidthLbl.AutoSize = true;
+            this.MaxWidthLbl.Location = new System.Drawing.Point(62, 15);
+            this.MaxWidthLbl.Name = "MaxWidthLbl";
+            this.MaxWidthLbl.Size = new System.Drawing.Size(122, 13);
+            this.MaxWidthLbl.TabIndex = 0;
+            this.MaxWidthLbl.Text = "Max Width of Room (ft.):";
+            // 
+            // MaxLengthLbl
+            // 
+            this.MaxLengthLbl.AutoSize = true;
+            this.MaxLengthLbl.Location = new System.Drawing.Point(57, 47);
+            this.MaxLengthLbl.Name = "MaxLengthLbl";
+            this.MaxLengthLbl.Size = new System.Drawing.Size(127, 13);
+            this.MaxLengthLbl.TabIndex = 1;
+            this.MaxLengthLbl.Text = "Max Length of Room (ft.):";
+            // 
+            // HardwoodPriceLbl
+            // 
+            this.HardwoodPriceLbl.AutoSize = true;
+            this.HardwoodPriceLbl.Location = new System.Drawing.Point(52, 76);
+            this.HardwoodPriceLbl.Name = "HardwoodPriceLbl";
+            this.HardwoodPriceLbl.Size = new System.Drawing.Size(132, 13);
+            this.HardwoodPriceLbl.TabIndex = 2;
+            this.HardwoodPriceLbl.Text = "Hardwood Price (sq. yard):";
+            // 
+            // UnderlaymentLbl
+            // 
+            this.UnderlaymentLbl.AutoSize = true;
+            this.UnderlaymentLbl.Location = new System.Drawing.Point(21, 105);
+            this.UnderlaymentLbl.Name = "UnderlaymentLbl";
+            this.UnderlaymentLbl.Size = new System.Drawing.Size(163, 13);
+            this.UnderlaymentLbl.TabIndex = 3;
+            this.UnderlaymentLbl.Text = "Underlayment  (1 = YES, 0 = NO)";
+            // 
+            // RoomLbl
+            // 
+            this.RoomLbl.AutoSize = true;
+            this.RoomLbl.Location = new System.Drawing.Point(36, 135);
+            this.RoomLbl.Name = "RoomLbl";
+            this.RoomLbl.Size = new System.Drawing.Size(148, 13);
+            this.RoomLbl.TabIndex = 4;
+            this.RoomLbl.Text = "First Room  (1 = YES, 0 = NO)";
+            // 
+            // MaxWidthTxtBox
+            // 
+            this.MaxWidthTxtBox.Location = new System.Drawing.Point(204, 8);
+            this.MaxWidthTxtBox.Name = "MaxWidthTxtBox";
+            this.MaxWidthTxtBox.Size = new System.Drawing.Size(109, 20);
+            this.MaxWidthTxtBox.TabIndex = 5;
+            // 
+            // MaxLengthTxtBox
+            // 
+            this.MaxLengthTxtBox.Location = new System.Drawing.Point(204, 40);
+            this.MaxLengthTxtBox.Name = "MaxLengthTxtBox";
+            this.MaxLengthTxtBox.Size = new System.Drawing.Size(109, 20);
+            this.MaxLengthTxtBox.TabIndex = 6;
+            // 
+            // HardwoodPriceTxtBox
+            // 
+            this.HardwoodPriceTxtBox.Location = new System.Drawing.Point(204, 69);
+            this.HardwoodPriceTxtBox.Name = "HardwoodPriceTxtBox";
+            this.HardwoodPriceTxtBox.Size = new System.Drawing.Size(109, 20);
+            this.HardwoodPriceTxtBox.TabIndex = 7;
+            // 
+            // UnderlaymentTxtBox
+            // 
+            this.UnderlaymentTxtBox.Location = new System.Drawing.Point(204, 98);
+            this.UnderlaymentTxtBox.Name = "UnderlaymentTxtBox";
+            this.UnderlaymentTxtBox.Size = new System.Drawing.Size(109, 20);
+            this.UnderlaymentTxtBox.TabIndex = 8;
+            // 
+            // FirstRoomTxtBox
+            // 
+            this.FirstRoomTxtBox.Location = new System.Drawing.Point(204, 128);
+            this.FirstRoomTxtBox.Name = "FirstRoomTxtBox";
+            this.FirstRoomTxtBox.Size = new System.Drawing.Size(109, 20);
+            this.FirstRoomTxtBox.TabIndex = 9;
+            // 
+            // SqYardsLbl
+            // 
+            this.SqYardsLbl.AutoSize = true;
+            this.SqYardsLbl.Location = new System.Drawing.Point(128, 180);
+            this.SqYardsLbl.Name = "SqYardsLbl";
+            this.SqYardsLbl.Size = new System.Drawing.Size(56, 13);
+            this.SqYardsLbl.TabIndex = 10;
+            this.SqYardsLbl.Text = "Sq. Yards:";
+            // 
+            // HardwoodCostLbl
+            // 
+            this.HardwoodCostLbl.AutoSize = true;
+            this.HardwoodCostLbl.Location = new System.Drawing.Point(104, 215);
+            this.HardwoodCostLbl.Name = "HardwoodCostLbl";
+            this.HardwoodCostLbl.Size = new System.Drawing.Size(80, 13);
+            this.HardwoodCostLbl.TabIndex = 11;
+            this.HardwoodCostLbl.Text = "Hardwood Cost";
+            // 
+            // UnderlayCostLbl
+            // 
+            this.UnderlayCostLbl.AutoSize = true;
+            this.UnderlayCostLbl.Location = new System.Drawing.Point(108, 248);
+            this.UnderlayCostLbl.Name = "UnderlayCostLbl";
+            this.UnderlayCostLbl.Size = new System.Drawing.Size(76, 13);
+            this.UnderlayCostLbl.TabIndex = 12;
+            this.UnderlayCostLbl.Text = "Underlay Cost:";
+            // 
+            // LaborLbl
+            // 
+            this.LaborLbl.AutoSize = true;
+            this.LaborLbl.Location = new System.Drawing.Point(123, 284);
+            this.LaborLbl.Name = "LaborLbl";
+            this.LaborLbl.Size = new System.Drawing.Size(61, 13);
+            this.LaborLbl.TabIndex = 13;
+            this.LaborLbl.Text = "Labor Cost:";
+            // 
+            // TotalCostLbl
+            // 
+            this.TotalCostLbl.AutoSize = true;
+            this.TotalCostLbl.Location = new System.Drawing.Point(126, 319);
+            this.TotalCostLbl.Name = "TotalCostLbl";
+            this.TotalCostLbl.Size = new System.Drawing.Size(58, 13);
+            this.TotalCostLbl.TabIndex = 14;
+            this.TotalCostLbl.Text = "Total Cost:";
+            // 
+            // TotalSqYards
+            // 
+            this.TotalSqYards.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalSqYards.Location = new System.Drawing.Point(204, 168);
+            this.TotalSqYards.Name = "TotalSqYards";
+            this.TotalSqYards.Size = new System.Drawing.Size(108, 25);
+            this.TotalSqYards.TabIndex = 15;
+            // 
+            // TotalHardwoodCost
+            // 
+            this.TotalHardwoodCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalHardwoodCost.Location = new System.Drawing.Point(204, 203);
+            this.TotalHardwoodCost.Name = "TotalHardwoodCost";
+            this.TotalHardwoodCost.Size = new System.Drawing.Size(108, 25);
+            this.TotalHardwoodCost.TabIndex = 16;
+            // 
+            // TotalUnderlayCost
+            // 
+            this.TotalUnderlayCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalUnderlayCost.Location = new System.Drawing.Point(204, 236);
+            this.TotalUnderlayCost.Name = "TotalUnderlayCost";
+            this.TotalUnderlayCost.Size = new System.Drawing.Size(108, 25);
+            this.TotalUnderlayCost.TabIndex = 17;
+            // 
+            // TotalLaborCost
+            // 
+            this.TotalLaborCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalLaborCost.Location = new System.Drawing.Point(204, 272);
+            this.TotalLaborCost.Name = "TotalLaborCost";
+            this.TotalLaborCost.Size = new System.Drawing.Size(108, 25);
+            this.TotalLaborCost.TabIndex = 18;
+            // 
+            // GrandTotalCost
+            // 
+            this.GrandTotalCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.GrandTotalCost.Location = new System.Drawing.Point(204, 307);
+            this.GrandTotalCost.Name = "GrandTotalCost";
+            this.GrandTotalCost.Size = new System.Drawing.Size(108, 25);
+            this.GrandTotalCost.TabIndex = 19;
+            // 
+            // CalcBtn
+            // 
+            this.CalcBtn.Location = new System.Drawing.Point(154, 359);
+            this.CalcBtn.Name = "CalcBtn";
+            this.CalcBtn.Size = new System.Drawing.Size(158, 31);
+            this.CalcBtn.TabIndex = 20;
+            this.CalcBtn.Text = "Calculate Estimate";
+            this.CalcBtn.UseVisualStyleBackColor = true;
+            this.CalcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.CalcBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 450);
+            this.Controls.Add(this.CalcBtn);
+            this.Controls.Add(this.GrandTotalCost);
+            this.Controls.Add(this.TotalLaborCost);
+            this.Controls.Add(this.TotalUnderlayCost);
+            this.Controls.Add(this.TotalHardwoodCost);
+            this.Controls.Add(this.TotalSqYards);
+            this.Controls.Add(this.TotalCostLbl);
+            this.Controls.Add(this.LaborLbl);
+            this.Controls.Add(this.UnderlayCostLbl);
+            this.Controls.Add(this.HardwoodCostLbl);
+            this.Controls.Add(this.SqYardsLbl);
+            this.Controls.Add(this.FirstRoomTxtBox);
+            this.Controls.Add(this.UnderlaymentTxtBox);
+            this.Controls.Add(this.HardwoodPriceTxtBox);
+            this.Controls.Add(this.MaxLengthTxtBox);
+            this.Controls.Add(this.MaxWidthTxtBox);
+            this.Controls.Add(this.RoomLbl);
+            this.Controls.Add(this.UnderlaymentLbl);
+            this.Controls.Add(this.HardwoodPriceLbl);
+            this.Controls.Add(this.MaxLengthLbl);
+            this.Controls.Add(this.MaxWidthLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MaxWidthLbl;
+        private System.Windows.Forms.Label MaxLengthLbl;
+        private System.Windows.Forms.Label HardwoodPriceLbl;
+        private System.Windows.Forms.Label UnderlaymentLbl;
+        private System.Windows.Forms.Label RoomLbl;
+        private System.Windows.Forms.TextBox MaxWidthTxtBox;
+        private System.Windows.Forms.TextBox MaxLengthTxtBox;
+        private System.Windows.Forms.TextBox HardwoodPriceTxtBox;
+        private System.Windows.Forms.TextBox UnderlaymentTxtBox;
+        private System.Windows.Forms.TextBox FirstRoomTxtBox;
+        private System.Windows.Forms.Label SqYardsLbl;
+        private System.Windows.Forms.Label HardwoodCostLbl;
+        private System.Windows.Forms.Label UnderlayCostLbl;
+        private System.Windows.Forms.Label LaborLbl;
+        private System.Windows.Forms.Label TotalCostLbl;
+        private System.Windows.Forms.Label TotalSqYards;
+        private System.Windows.Forms.Label TotalHardwoodCost;
+        private System.Windows.Forms.Label TotalUnderlayCost;
+        private System.Windows.Forms.Label TotalLaborCost;
+        private System.Windows.Forms.Label GrandTotalCost;
+        private System.Windows.Forms.Button CalcBtn;
+    }
+}
+
---
+++ Program 1/Form1.cs
@@ -0,0 +1,86 @@
+﻿// Grade ID: R7639
+// Program 1
+// Due Date : 9/22/2020
+// Course: CIS 199-01
+// This program calculates the total estimate cost of placing floors and underlayment
+// As well as whether this is the first room or not
+// Also calculating the total labor cost
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcBtn_Click(object sender, EventArgs e)
+        {
+            const double LABOR_PER_YARD = 3.25; // Labor cost
+            const double UNDERLAYMENT_PER_YARD = 4.25; // Underlay Cost
+            const double FIRST_ROOM = 50.00; // First Room price
+            const double SQFT_SQYD = 9.0; // Used to calculate SQ Yards
+            const double EXCESS = .10; // The excess for hardwood
+            // A set of named constants used for calculations
+
+            double width, length, hardwoodprice, SQYards, hardwoodcost, underlaycost, laborcost, totalcost; // All of these have decimals 
+            int underlayment, firstroom; // single digit in yes or no(1 or 0)
+
+            width = double.Parse(MaxWidthTxtBox.Text); // Allows input of width
+
+            length = double.Parse(MaxLengthTxtBox.Text); // Allows input of Length
+
+            hardwoodprice = double.Parse(HardwoodPriceTxtBox.Text); // Allows input of the hardwood price
+
+            underlayment = int.Parse(UnderlaymentTxtBox.Text); // Allows input of whether undelay is needed or not
+
+            firstroom = int.Parse(FirstRoomTxtBox.Text); // Allows input on whether this is the first room
+
+            SQYards = (width * length) / SQFT_SQYD;
+            TotalSqYards.Text = $"{SQYards:F1}";
+            // Calculates Sq Yards and sets it to currency
+
+            hardwoodcost = ((SQYards * hardwoodprice) * EXCESS) + (SQYards * hardwoodprice);
+            TotalHardwoodCost.Text = $"{hardwoodcost:C}";
+            // Calculates cost of Hardwood including the excess amount and sets it to currency
+
+            if (underlayment == 1)
+                underlaycost = SQYards * UNDERLAYMENT_PER_YARD;
+            else
+                underlaycost = SQYards * 0;
+            TotalUnderlayCost.Text = $"{underlaycost:C}";
+            // Using if-else statement to define underlayment cost
+            // Had trouble using a regular if statement thats why if-else was used here
+            // using if statement gave me CS0165 error
+             
+            
+
+            if (firstroom == 1)
+                laborcost = (SQYards * LABOR_PER_YARD) + FIRST_ROOM;
+            else
+                laborcost = SQYards * LABOR_PER_YARD;
+            TotalLaborCost.Text = $"{laborcost:C}";
+            // using if else statement to calculate labor cost 
+            // differentiates between having the first room or not
+
+            totalcost = hardwoodcost + underlaycost + laborcost;
+            GrandTotalCost.Text = $"{totalcost:C}";
+            // calculates total cost in a currency format
+
+ 
+        }
+    }
+}
